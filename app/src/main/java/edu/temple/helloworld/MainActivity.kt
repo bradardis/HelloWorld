18bf576663be3053e47f52dbdcbc2620563e7e74
Bradley Ardis <tui38054@temple.edu>
2022-02-01 14:26:18 -0500

    c

---
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -0,0 +1,21 @@
+package edu.temple.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun buttonClick(view: View) {
+
+        textview.text = "you clicked the button"
+    }
+
+}
